115a80501fe9a789d5d3d7797f3b040fa1436a9b
rahulpatle2727 <rahul23131420@gmail.com>
2022-04-11 22:54:20 +0530

    delete

--- fac.sh
+++
@@ -1,9 +0,0 @@
-echo "Enter a number " 
-read num
-fact=1
- 
-for (( i=2; i<$num; i++ ))
-do
-  fact=$(( fact*i ))
-done 
-echo $fact
--- factorial.sh
+++
@@ -1,9 +0,0 @@
-echo "enter the no"
-# read num
-res = 1
-for((i=1; i<=4; i++))
-do
-	res = `expr $i * $res`
-done 
-echo "the factofial is" 
-echo "$res"
