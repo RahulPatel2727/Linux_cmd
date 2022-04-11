27649a8444afb9ba32d0a71cabe296d819426caf
rahulpatle2727 <rahul23131420@gmail.com>
2022-04-11 22:40:37 +0530

    zsh shell programs

--- check_two_no_are_equal_or_not.sh
+++ check_two_no_are_equal_or_not.sh
@@ -2,7 +2,7 @@ echo enter first no
 read a
 echo enter second no
 read b
-if [$a -eq $b ];
+if [ $a -eq $b ]
 then
 	echo yes
 else
--- fac.sh
+++ fac.sh
@@ -1,9 +1,9 @@
-read -p "Enter a number" num
+echo "Enter a number " 
+read num
 fact=1
  
-for((i=2;i<=num;i++))
-{
-  fact=$((fact*i))
-}
- 
+for (( i=2; i<$num; i++ ))
+do
+  fact=$(( fact*i ))
+done 
 echo $fact
